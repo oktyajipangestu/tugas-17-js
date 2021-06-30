bdc36e8dd8393f9504896a6452976c07283889f2
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-06-30 21:37:23 +0700

    Add files via upload

---
+++ tugas17-1.js
@@ -0,0 +1,8 @@
+let text1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+let text2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+
+console.log(text1.toLocaleLowerCase());
+console.log(text1.toLocaleUpperCase());
+console.log(text2.toLocaleLowerCase());
+console.log(text2.toLocaleUpperCase());
+
---
+++ tugas17-2.js
@@ -0,0 +1,3 @@
+let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+console.log(text.charCodeAt(0) + "-" + text.charCodeAt(1) + "-" + text.charCodeAt(2) + "-" + text.charCodeAt(3) + "-" + text.charCodeAt(4) + "-" + text.charCodeAt(5));
